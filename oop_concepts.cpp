02e90b1622be27f0d75178cade238233aef74004
tigerak1234 <4500.stkabirnav@gmail.com>
2022-11-08 22:27:02 +0530

    Create oop_concepts.cpp

---
+++ oop_concepts.cpp
@@ -0,0 +1,41 @@
+C++ : Object Oriented Programming (OOP)
+==> object based
+=> enhance security
+
+Object: everything in the world is an Object
+
+Class (Logical Entity) 
+
+ => Human 
+
+Object: (Physical Entity)
+ => Men
+ => Women
+ 
+ 5 major piller/concepts
+ 
+ 1. Class and object
+ 
+ ==> constructor/ destructor
+ 
+ 2. Encapsulation
+ 3. Inheritance
+ ==> virtual base Class
+ 
+ 4. Polymorphism
+ 5. Abstraction
+ 
+ ==> friend class
+ ==> friend fucntions
+ ==> C++ templates
+ ==> file handling
+ 
+ Basic Structure:
+ 	
+ 	#include<iostream> // input/output stream
+ 	using namespace std; // for cout (printf) and cin (scanf)
+ 	int main()
+ 	{
+ 		
+ 		return 0;
+	 }
