975311831d8d93345422b4a800bec7613f058c68
tigerak1234 <4500.stkabirnav@gmail.com>
2022-11-08 21:06:28 +0530

    Create diff_btw_oop_and_pop.cpp

---
+++ diff_btw_oop_and_pop.cpp
@@ -0,0 +1,27 @@
+OOP
+=>Object oriented.
+=>Program is divided into objects.
+=>Inheritance property is used.
+=>It uses access specifier.
+=>Encapsulation is used to hide the data.
+=>Concept of virtual function.
+=>Object functions are linked through message passing.
+=>Adding new data and functions is easy
+=>The existing code can be reused.
+=>use for solving big problems.
+=>Bottom-up approach.
+=>C++, Java.
+
+POP
+=>Structure oriented.
+=>Program is divided into functions.
+=>Top-down approach.
+=>Inheritance is not allowed.
+=>It doesn’t use access specifier.
+=>No data hiding.
+=>No virtual function.
+=>Parts of program are linked through parameter passing
+=>No code reusability.
+=>Expanding new data and functions is not easy.
+=>Not suitable for solving big problems.
+=>C, Pascal.
